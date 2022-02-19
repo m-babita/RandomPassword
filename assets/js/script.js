4369b3f72695e00ee8b483f3912b7575176b62bc
m-babita <babita.majumdar.1@gmail.com>
2022-02-19 16:41:50 +0530

    document and style is added

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//selectors
+const generateBtn = document.querySelector(".generate__btn");
+const passwords = document.querySelector(".passwords")
+
+//event listeners
+generateBtn.addEventListener("click", gernerate)
+
+//functions
+function generate(){
+    
+}
